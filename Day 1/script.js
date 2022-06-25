aa11e053c0355fcaa4ffb92eeeaf1be8eba9c116
Prashant Puri <ppuri7069@gmail.com>
2022-06-25 15:46:08 +0545

    Simple If Statement

---
+++ Day 1/script.js
@@ -0,0 +1,5 @@
+// A simple if statement
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is fun!');
+40 + 8 + 23 - 10;   //console log ain't define so nowhere to print
+console.log(40 + 8 + 23 - 10);  // Prints in console
