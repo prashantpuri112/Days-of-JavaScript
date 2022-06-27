53d1c421ff74ef914a004c4cfc615d673c36823b
Prashant Puri <ppuri7069@gmail.com>
2022-06-27 21:56:51 +0545

    Understanding Variables and Values

---
+++ Day 1/variable.js
@@ -0,0 +1,36 @@
+// variable : It basically a box into which we can store a value
+console.log("Prashant");
+console.log(50);
+
+//CamelCase
+let firstName = "bob";
+console.log(firstName);
+
+let firstNamePerson = "bob";
+console.log(firstNamePerson);
+
+// Variable name cannot be start with number, it can only contails number letter underscope (_) or dollor sign ($)
+// let 5year = 7;
+// let Prashant&Puri ='pp', these are the illegal symbol used to declare variable
+
+let Prashant_Puri = 'pp' //correct declaration
+
+// let new = 40;
+// let function = 50;
+// let name = 90;
+// these are the reserved keywords in javascript so we cannot use it directly to declare variable
+let $function = 50; //instead we can add symbols to declare
+
+
+let Person = 'Prashant' //we should not start a variable name with the upper case letter as it is used for specific usecase in JS for OOP
+let PI = 3.14; // as the value of pie is constant so I have used Uppercase in all letter
+
+// Suggested approch to implement as it is descriptive
+let myFirstName = 'Prashant';
+let myFamilyName = 'Puri';
+
+// Not to implement like this cause it is not descriptive
+let name = 'prashant';
+let cast = 'puri';
+
+console.log(myFirstName);
