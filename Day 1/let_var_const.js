11f176d38443cfda2de239f465c28f78385088e6
Prashant Puri <ppuri7069@gmail.com>
2022-06-28 13:19:03 +0545

    LET VAR CONST

---
+++ Day 1/let_var_const.js
@@ -0,0 +1,18 @@
+let age = 22;
+age = 25;
+
+
+//const is not supposed to be change
+const birthYear = 2002; //immutable variable
+// birthYear = 1990;
+
+// const job;
+
+//suggest to completely avoid the use of var
+
+var job = 'programmer';
+job = 'CA';
+
+//terrible way
+lastname = 'Puri'
+console.log(lastname)
