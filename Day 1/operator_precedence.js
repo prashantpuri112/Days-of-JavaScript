f19b2f2793b629966f0749c18664e5a05c75a768
Prashant Puri <ppuri7069@gmail.com>
2022-06-28 14:29:07 +0545

    Understanding Operator Precedence

---
+++ Day 1/operator_precedence.js
@@ -0,0 +1,13 @@
+const now = 2022
+const agePrashant = now - 2002;
+const agePuri = now - 1990;
+
+console.log(now - 2002 > now - 1990);
+console.log(25 - 10 - 5); //left to right
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (agePrashant + agePuri) / 2
+console.log(agePrashant, agePuri, averageAge);
