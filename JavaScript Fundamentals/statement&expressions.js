510a6018ad3c199a8ffaf7d92ef2c3b9b6c82834
Prashant Puri <ppuri7069@gmail.com>
2022-07-13 00:31:09 +0545

    Understanding Statement & Expressions

---
+++ JavaScript Fundamentals/statement&expressions.js
@@ -0,0 +1,12 @@
+/* Expression: It is an piece of code that produces a valuse i.e (1+1=2), It is expression as it is gonna produce a value
+Statement: It is like a bigger piece of a code that is executed and which doesn't produces values on itself.
+
+Declaration is like a complete sentences and expressions are like the words that make up the sentences
+*/
+
+if (23 > 10) {
+    const string = '23 is bigger';          // Does not produce a value
+}                                           // Whenever a things is end with a semicolon it is an statement (It is like a complete sentence)
+
+const me = 'Prashant'
+console.log(`I'm ${2022 - 2002} year old ${me}`);
