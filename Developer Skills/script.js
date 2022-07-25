96d23c24b0bf6fa40db5a173bf953d7ac3220989
Prashant Puri <ppuri7069@gmail.com>
2022-07-25 12:01:46 +0545

    Setting up prettier and skilled up developer skill

---
+++ Developer Skills/script.js
@@ -0,0 +1,9 @@
+/*Practicing Developer skills, setting up editor and configuring Prettier
+ */
+
+'use strict';
+
+const xasda = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2022 - birthYear;
