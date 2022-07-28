2e7f1ab84ce0f5a1db3650c7803844f84f5507cb
Prashant Puri <ppuri7069@gmail.com>
2022-07-29 01:15:21 +0545

    Handling Click Events

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,7 +1,6 @@
 'use strict';
-
-/*querySelector is basically a method that's available on the document object
-For Class we use (.) Dot, For ID we use (#)*/
+/* 
+//querySelector is basically a method that's available on the document object For Class we use (.) Dot, For ID we use (#)
 
 //In this line we started by reading the content of the element
 console.log(document.querySelector('.message').textContent);
@@ -19,3 +18,33 @@ document.querySelector('.score').textContent = 98;
 console.log(document.querySelector('.guess').value); //read
 document.querySelector('.guess').value = 24; //overwrite 
 console.log(document.querySelector('.guess').value); //re-read
+
+*/
+
+/* To make our code reacts to something that happens in the DOM and for that we need to use an event listener
+Event is something that happens on the page. For example:
+A mouse click
+A mouse moving
+A key press
+
+With an event listner, we can wait for a certain event to happen and than react to it
+*/
+
+
+
+//AddEventListener is the best one and also the most used one
+document.querySelector('.check').addEventListener('click', function () {        //We have to pass a second argument with a function value
+    const guess = Number(document.querySelector('.guess').value);        //This function simply contains the code that we want to execute whenever the event happens
+    console.log(guess, typeof guess);
+
+    if (!guess) {   //For falsy value 0
+        console.log(document.querySelector('.message').textContent = 'No Number!');
+    }
+});
+/* we do not call this function here anywhere, we only define the function here and then pass it into the event handler
+but it is the JavaScript engine who will call this function as soon as the event happens.
+The function will not be called immediately once the script is executed
+This function will only be called as soon as the event happens*/
+
+
+
