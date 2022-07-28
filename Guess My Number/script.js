aa1f145562bbee03eb0383729c0e6955a4e2bba9
Prashant Puri <ppuri7069@gmail.com>
2022-07-28 13:09:35 +0545

    First Project & Introduction to DOM

---
+++ Guess My Number/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/*querySelector is basically a method that's available on the document object
+For Class we use (.) Dot, For ID we use (#)*/
+
+console.log(document.querySelector('.message').textContent);
+
