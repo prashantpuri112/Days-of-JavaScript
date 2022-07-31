ee38cc66b52ca05c9037e361b288d69238807613
Prashant Puri <ppuri7069@gmail.com>
2022-07-31 16:40:54 +0545

    Working with classes

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -2,11 +2,35 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModel = document.querySelector('.close-model');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal); //Whenever we use query selector with a selector, which actually matches multiple elements only the first one will get selected
+// console.log(btnOpenModal); //Whenever we use query selector with a selector, which actually matches multiple elements only the first one will get selected
 
 //To solve the limitation we can use "document.querySelectorAll"
 
+//Function to close modal
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+//Function to close modal
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+//Not calling function just declaring or defining it, and tell JavaScript, "Hey, whenever a click happens on one of these buttons than only call that function, which is specified above"
 for (let i = 0; i < btnOpenModal.length; i++)
-    console.log(btnOpenModal[i].textContent);
+    btnOpenModal[i].addEventListener('click', openModal);
+
+/*
+console.log('Button clicked');
+modal.classList.remove('hidden');       //Removing hidden classes
+overlay.classList.remove('hidden');
+
+ modal.style.display = 'block';
+*/
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
