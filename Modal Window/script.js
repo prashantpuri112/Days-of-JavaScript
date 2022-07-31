bf8cd1efd3b38b50f6a2dc1b0bf3d998a1afa683
Prashant Puri <ppuri7069@gmail.com>
2022-07-31 13:38:55 +0545

    Project 2 Starter

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-model');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal); //Whenever we use query selector with a selector, which actually matches multiple elements only the first one will get selected
+
+//To solve the limitation we can use "document.querySelectorAll"
+
+for (let i = 0; i < btnOpenModal.length; i++)
+    console.log(btnOpenModal[i].textContent);
