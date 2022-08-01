668af57957743471ebad7193f07dbbfc566c7095
Prashant Puri <ppuri7069@gmail.com>
2022-08-02 00:17:40 +0545

    Holding Current Score

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -21,34 +21,62 @@ diceEl.classList.add('.hidden');
 const scores = [0, 0];
 let currentScore = 0; //Else condition in line 38, if the dice roll 1 it becames 0
 let activePlayer = 0;
+let playing = true;
+
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;  //reassigning the active player, if it is player 0 switching it to the player 1 (ViceVersa)
+
+    //Basically switching background shades to define active player
+    player0El.classList.toggle('player--active')    //toggle will add the class if it is not there and remove it, if it is there
+    player1El.classList.toggle('player--active')
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
+    if (playing) {
+        //1. Generating a randon dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    //1. Generating a randon dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-
-    //2. Display Dice
-    diceEl.classList.remove('.hidden');
-    diceEl.src = `dice-${dice}.png`;  //Manipulating Src property
+        //2. Display Dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;  //Manipulating Src property
 
 
-    //3. Check for rolled 1
-    if (dice !== 1) {
-        //Add dice to current score
+        //3. Check for rolled 1
+        if (dice !== 1) {
+            //Add dice to current score
 
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore
 
-    } else {
-        //switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;  //reassigning the active player, if it is player 0 switching it to the player 1 (ViceVersa)
-
-        //Basically switching background shades to define active player
-        player0El.classList.toggle('player--active')    //toggle will add the class if it is not there and remove it, if it is there
-        player1El.classList.toggle('player--active')
-
+        } else {
+            //switch to next player
+            switchPlayer();
+        }
     }
 });
+
+btnHold.addEventListener('click', function () {
+    if (playing) {
+        //1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
+        // scores[1]=scores[1]+currentScore
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        //2. Check if player's score is >=100
+        if (scores[activePlayer] >= 20) {
+            //Finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        } else {
+            //Switch to the next player
+            switchPlayer();
+        }
+    }
+})
