aa9e6f61dbce592b20286cd3b3c7ed443f9ac52f
Prashant Puri <ppuri7069@gmail.com>
2022-08-01 22:30:36 +0545

    Pig Game Starter

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');     /*getElementById is supposed to be a litter bit faster than querySelector
+(Only revelent if you're selecting like thousands of elements at once) */
+const diceEl = document.querySelector('.dice');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
