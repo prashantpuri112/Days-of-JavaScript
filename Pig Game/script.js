8173ef95541df3cc1855a7708050a2d953b851fc
Prashant Puri <ppuri7069@gmail.com>
2022-08-01 23:36:44 +0545

    Switching the Active Player

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');     /*getElementById is supposed to be a litter bit faster than querySelector
 (Only revelent if you're selecting like thousands of elements at once) */
@@ -16,7 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('.hidden');
 
-let currentScore = 0;
+const scores = [0, 0];
+let currentScore = 0; //Else condition in line 38, if the dice roll 1 it becames 0
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -32,12 +36,19 @@ btnRoll.addEventListener('click', function () {
     //3. Check for rolled 1
     if (dice !== 1) {
         //Add dice to current score
-        currentScore += dice;
-        current0El.textContent = currentScore;
 
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
 
     } else {
         //switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;  //reassigning the active player, if it is player 0 switching it to the player 1 (ViceVersa)
+
+        //Basically switching background shades to define active player
+        player0El.classList.toggle('player--active')    //toggle will add the class if it is not there and remove it, if it is there
+        player1El.classList.toggle('player--active')
 
     }
 });
