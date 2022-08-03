6bf4c3e7339ca574ab5f4ea8cd83aff08272f2c9
Prashant Puri <ppuri7069@gmail.com>
2022-08-04 00:36:26 +0545

    Resetting the Game

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -8,12 +8,15 @@ const score1El = document.getElementById('score--1');     /*getElementById is su
 (Only revelent if you're selecting like thousands of elements at once) */
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//Starting conditions
+/*
+// Starting conditions
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('.hidden');
@@ -22,7 +25,32 @@ const scores = [0, 0];
 let currentScore = 0; //Else condition in line 38, if the dice roll 1 it becames 0
 let activePlayer = 0;
 let playing = true;
+*/
 
+let scores, currentScore, activePlayer, playing;
+// Starting conditions
+
+const init = function () {
+
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('.hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+init(); //Executing all the code inside the function
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -67,7 +95,7 @@ btnHold.addEventListener('click', function () {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         //2. Check if player's score is >=100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             //Finish the game
             playing = false;
             diceEl.classList.add('hidden');
@@ -79,4 +107,6 @@ btnHold.addEventListener('click', function () {
             switchPlayer();
         }
     }
-})
+});
+
+btnNew.addEventListener('click', init);
