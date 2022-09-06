20d085255a67783538b04a3f1325b08c3644d3f7
Prashant Puri <ppuri7069@gmail.com>
2022-09-06 13:37:56 +0545

    Destructing Objects

---
+++ Data Structures Operators/destructing_objects.js
@@ -0,0 +1,71 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order received!${this.starterMenu[starterIndex]}and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+};
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'via del sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+    address: 'via del sole, 21',
+    starterIndex: 1
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const { name: restaurantName, openingHours: hours, categories: tages } = restaurant;
+console.log(restaurantName, hours, tages);
+
+//Default values
+const { menu = [], starterMenu: starter = [] } = restaurant
+console.log(menu, starter);
+
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj); //Wrapping the destructing assignment into parenthesis
+console.log(a, b);
+
+//Nested Objects
+const { fri: { open: o, close: c } } = openingHours;
+console.log(o, c);
+
+
+
+
+
+
+
