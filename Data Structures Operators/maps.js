e15be9e7180dab10dfdf6e859ba52e4a52d78577
Prashant Puri <ppuri7069@gmail.com>
2022-09-24 01:55:38 +0545

    Maps: Fundamentals

---
+++ Data Structures Operators/maps.js
@@ -0,0 +1,89 @@
+/* Maps are a lot more useful than sets.
+
+So what exactly is a map?
+
+Well, it's certainly not the same thing that we use in the real life to find our ways around.
+Now, instead in JavaScript, a map is a data structure that we can use to map values to keys.
+
+So, just like an object data is stored in key value pairs in maps.
+Now, the big difference between objects and maps is that in maps, the keys can have any type and this can be huge.
+
+So, in objects, the keys are basically always strings. But in maps, we can have any type of key.
+
+It could even be objects, or arrays, or other maps. So, that can lead to some really great and advanced stuff. */
+
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    //ES6 enhanced object literals
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is your declicious pasta with ${ing1},${ing2},${ing3}`);
+    },
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    }
+};
+
+const rest = new Map();
+rest.set('name', 'Classico Italino');
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open', 11).set('close', 23).set(true, 'We are open :D').set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr)); //using objects as map keys
+
+
+
+
