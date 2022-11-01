e52a42005ec8002b201c8ed880f905b9dcc9365e
Prashant Puri <ppuri7069@gmail.com>
2022-11-01 23:28:42 +0545

    The find Method

---
+++ Working with Arrays/find_method.js
@@ -0,0 +1,55 @@
+/* Find Method: So as the name says, we can use the Find method to retrieve one element  of an array based on a condition.
+ Find is basically just another method that loops over the array but then it does something different. The Find method is a bit similar to the Filter method,
+but there are two fundamental differences.
+
+    First Filter returns all the elements that match the condition while the Find method only returns the first one
+    Second and even more important, the Filter method returns a new array while Find only returns the element itselfand not an array
+*/
+
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+};
+
+const account2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+
+const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
+
+const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
